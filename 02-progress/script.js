c8ad246dde760164513294fb22f226bda646f341
zhuangzhuang-huang <cau1403090523@gmail.com>
2021-02-23 12:07:03 +0800

    add all

---
+++ 02-progress/script.js
@@ -0,0 +1,58 @@
+const progress = document.getElementById("progress");
+const next = document.getElementById("next");
+const prev = document.getElementById("prev");
+const circles = document.querySelectorAll(".circle");
+
+let count = 1;
+next.addEventListener("click",event => {
+    count++;
+
+    if(count>circles.length){
+        count = circles.length;
+    }
+
+    console.log(count);
+    console.log(circles);
+    _update();
+    
+
+})
+
+
+prev.addEventListener('click',event => {
+    count--;
+
+    if(count < 1){
+        count = 1
+    }
+
+    console.log(count);
+    console.log(circles);
+    _update();
+})
+
+
+function _update(){
+    circles.forEach((circle,index) => {
+        if(index<count){
+            circle.classList.add('active');
+        }else{
+            circle.classList.remove("active");
+        }
+    })
+
+    //  进度条
+    const precentWidth = ((count-1) / (circles.length-1))*100;
+    console.log(precentWidth)
+    progress.style.width = precentWidth+"%";
+
+    // 更新btn状态
+    if(count === circles.length){
+        next.disabled = true;
+    }else if(count === 1){
+        prev.disabled = true;
+    }else{
+        next.disabled = false;
+        prev.disabled = false;
+    }
+}
